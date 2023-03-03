8a027d87cfd5ee3717a0318ae4be2405561505fe
Sebastian Sala <sebastian_orko@hotmail.com>
2023-03-03 15:01:46 -0600

    primer avance

---
+++ proyecto/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { AdminComponent } from './admin/admin.component';
+const routes: Routes = [
+  {path: 'index', component: IndexComponent},
+  {path: 'admin', component: AdminComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
