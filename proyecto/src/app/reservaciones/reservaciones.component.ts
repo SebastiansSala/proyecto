085d9a6394162ee19998eecbbea40bdcae389cbb
Sebastian Sala <sebastian_orko@hotmail.com>
2023-03-08 17:06:32 -0600

    hola

---
+++ proyecto/src/app/reservaciones/reservaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservaciones',
+  templateUrl: './reservaciones.component.html',
+  styleUrls: ['./reservaciones.component.css']
+})
+
+export class ReservacionesComponent {
+
+  constructor(){
+  }
+
+
+}
